fe7dc2799d433012e20deedf95548e504251d84e
Yoshiya Sugimoto <y.sugimoto@jxpress.net>
2020-09-07 00:56:01 +0900

    impression page

---
+++ mybook/cms/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+from .models import Book
+
+
+class BookForm(ModelForm):
+    """書籍のフォーム"""
+
+    class Meta:
+        model = Book
+        fields = ("name", "publisher", "page")
--- mybook/cms/views.py
+++ mybook/cms/views.py
@@ -1,21 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
 from .models import Book
+from .forms import BookForm
+
+from django.views.generic.list import ListView
 
 
 def book_list(request):
     """書籍の一覧"""
     # return HttpResponse("書籍の一覧")
     books = Book.objects.all().order_by("id")
-    return render(request, 'cms/book_list.html', {"book": books})
+    return render(request, 'cms/book_list.html', {"books": books})
 
 
 def book_edit(request, book_id=None):
     """書籍の編集"""
-    return HttpResponse("書籍の編集")
+    # return HttpResponse("書籍の編集")
+    if book_id:
+        book = get_object_or_404(Book, pk=book_id)
+    else:
+        book = Book()
+
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            book = form.save(commit=False)
+            book.save()
+            return redirect('cms:book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, "cms/book_edit.html", dict(form=form, book_id=book_id))
 
 
 def book_del(request, book_id):
     """書籍の削除"""
     return HttpResponse("書籍の削除")
+
+
+class ImpressionList(ListView):
+    """乾燥の一覧"""
+    context_object_name = "impressions"
+    template_name = "cms/impression_list.html"
+    paginate_by = 2
+
+    def get(self, request, *args, **kwargs):
+        book = get_object_or_404(Book, pk=kwargs["book_id"])
+        impressions = book.impressions.all().order_by("id")
+        self.object_list = impressions
+
+        context = self.get_context_data(object_list=self.object_list, book=book)
+        return self.render_to_response(context)
